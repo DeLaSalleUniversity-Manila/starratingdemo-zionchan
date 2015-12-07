30f2250294b667bf4f869c3964de7ae65acdf407
zionchan <zion_chan@dlsu.edu.ph>
2015-12-07 09:52:54 +0800

    yay

---
+++ app/src/main/java/com/starratingdemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.starratingdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout rating;
+    private  CheckBox star;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rating = (LinearLayout) findViewById(R.id.ratings);
+
+        for (int i = 1; i <= 5; i++) {
+            star = (CheckBox) rating.findViewWithTag(String.valueOf(i));
+            star.setOnClickListener(starsListener);
+        }
+    }
+
+    private View.OnClickListener starsListener = new View.OnClickListener() {
+
+        public void onClick(View view) {
+
+            int tag = Integer.valueOf((String) view.getTag());
+
+            for (int i = 1; i <= tag; i++) {
+                star = (CheckBox) rating.findViewWithTag(String.valueOf(i));
+                star.setChecked(true);
+            }
+
+            for (int i = tag + 1; i <= 5; i++) {
+                star = (CheckBox) rating.findViewWithTag(String.valueOf(i));
+                star.setChecked(false);
+            }
+        }
+    };
+
+}
